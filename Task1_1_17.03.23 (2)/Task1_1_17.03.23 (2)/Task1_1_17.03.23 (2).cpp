f7225bdf74a0a109fd1178adccc66d3ae16e2755
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-03-16 20:52:54 +0200

    Add files via upload

---
+++ Task1_1_17.03.23 (2)/Task1_1_17.03.23 (2)/Task1_1_17.03.23 (2).cpp
@@ -0,0 +1,108 @@
+﻿/*Task1. Write a program that displays a line on the screen with a given symbol, vertical or horizontal,
+and the line can be displayed quickly, normally and slowly. Communication with the user can be organized through the menu.
+
+In this version you can enter anything in every field and the program will work properly.
+To be honest I spent a lot of time on this version, and I`m not sure if it was necesary. But I wanted to test my skills and learn something new*/
+ 
+
+#include <iostream>                  
+#include <chrono>// The code uses the this_thread::sleep_for function to pause the program execution for a certain amount of time, depending on the user inputted speed
+#include <thread>// this_thread::sleep_for takes a duration argument, which is created using the chrono library
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    //Basic variables
+    string symbol;
+    int direction, speed;
+    cout << "You can display the line vertical or horizontal, and quickly, normally and slowly." << endl;
+
+    // Start of endless loop
+    while (true) {
+
+        // 
+        cout << "\nEnter 0 to exit or any other symbol to continue: ";
+        cin >> symbol;
+
+        if (symbol[0] == '0') { // Exit
+
+            break;
+        }
+        // User inputs symbol
+        cout << "Enter a symbol: ";
+        cin >> symbol;
+
+        // User inpust direction
+        cout << "Enter direction (1 for vertical, 2 for horizontal): ";
+        cin >> direction;
+
+        // Check for valid direction input
+        if (direction != 1 && direction != 2) {
+
+            // When the user has entered invalid input we need to clear the input buffer before accepting new input
+            // this helps to avoid endless loop
+            cout << "Invalid direction." << endl;
+            cin.clear();  // Clear the input buffer
+            //sets the maximum number of characters to ignore, there is no limit to the number of characters to ignore.
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Discard any remaining input
+            continue;
+        }
+
+        // User inputs speed
+        cout << "Enter speed (1 for quickly, 2 for normally, 3 for slowly): ";
+        cin >> speed;
+
+        // Check for valid speed input
+        if (speed < 1 || speed > 3) {
+
+            cout << "Invalid speed." << endl;
+            cin.clear();  // Clear the input buffer
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Discard any remaining input
+            continue;
+        }
+
+        // Display line based on user input
+        if (direction == 1) { // Vertical
+
+            for (int i = 0; i < 10; i++) {
+
+                cout << symbol << endl;
+                if (speed == 1) {
+                    // Quickly
+                    this_thread::sleep_for(chrono::milliseconds(100));
+                }
+                else if (speed == 2) {
+                    // Normally
+                    this_thread::sleep_for(chrono::milliseconds(500));
+                }
+                else {
+                    // Slowly
+                    this_thread::sleep_for(chrono::seconds(1));
+                }
+            }
+        }
+        else { // Horizontal
+
+            for (int i = 0; i < 10; i++) {
+
+                cout << symbol;
+                if (speed == 1) {
+                    // Quickly
+                    this_thread::sleep_for(chrono::milliseconds(100));
+                }
+                else if (speed == 2) {
+                    // Normally
+                    this_thread::sleep_for(chrono::milliseconds(500));
+                }
+                else {
+                    // Slowly
+                    this_thread::sleep_for(chrono::seconds(1));
+                }
+            }
+            cout << endl;
+        }
+    }
+    
+}
---
+++ Task1_1_17.03.23/Task1_1_17.03.23/Task1_1_17.03.23.cpp
@@ -0,0 +1,122 @@
+﻿/*Task1. Write a program that displays a line on the screen with a given symbol, vertical or horizontal,
+and the line can be displayed quickly, normally and slowly. Communication with the user can be organized through the menu.
+
+The task did not have a condition what values should user enter, so I made two versions of the program. 
+Here is the first one (simple), where the program works as long as the user enters the correct data.
+An error is expected if the user enters an incorrect number for the "direction" and "speed" fields. 
+But if you enter a letter or a symbol in the indicated fields, endless loop starts, because the int does not accept anything but numbers. 
+I tried to deal with this problem in the second version of the task called "Task1_1_17.03.23 (2)".  */
+
+#include <iostream>
+#include "windows.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "You can display the line vertical or horizontal, and quickly, normally and slowly.\nEnter 0 in symbol value for exit." << endl; // Simple interface
+
+	int  direction, speed; 
+	string symbol;
+
+	// Endless loop
+	while (true) {
+		// User enters symbol
+		cout << "\nEnter the symbol to build the line: "; cin >> symbol;
+
+		if (symbol[0] == '0') { // Exit program
+
+			break;
+		}
+
+		// User enters direction
+		cout << "Choose direction: 1 - vertical; 2 - horizontal -> "; cin >> direction;
+
+			// Check if direction is valid
+			if ((direction != 1) && (direction != 2)) {
+
+				cout << "Wrong value!" << endl;
+			}
+			else {
+				// Vertical direction
+				if (direction == 1) {
+
+					// User inputs speed
+					cout << "Select speed: 1 - quickly; 2 - normally; 3 - slowly -> "; cin >> speed;
+
+					// Check if speed value is correct
+					if ((speed != 1) && (speed != 2)&& (speed != 3)) {
+
+						cout << "Wrong value!" << endl;
+					}
+
+
+					if (speed == 1) {
+
+						for (int i = 0; i <= 10; i++) { // quickly
+
+							cout << symbol << endl;
+							Sleep(50);
+						}
+					}
+
+					if (speed == 2) { // normally
+
+						for (int i = 0; i <= 10; i++) {
+
+							cout << symbol << endl;
+							Sleep(500);
+						}
+					}
+					if (speed == 3) { // slowly
+
+						for (int i = 0; i <= 10; i++) {
+
+							cout << symbol << endl;
+							Sleep(1000);
+						}
+					}
+
+				}
+				else { // Horizontal direction
+
+					// Check if speed value is correct
+					cout << "Select speed: 1 - quickly; 2 - normally; 3 - slowly -> "; cin >> speed;
+					if ((speed != 1) && (speed != 2) && (speed != 3)) {
+
+						cout << "Wrong value!" << endl;
+					}
+
+
+					if (speed == 1) {
+
+						for (int i = 0; i <= 10; i++) { // quickly
+
+							cout << symbol;
+							Sleep(50);
+						}
+					}
+					if (speed == 2) {// normally
+
+						for (int i = 0; i <= 10; i++) {
+
+							cout << symbol;
+							Sleep(500);
+						}
+					}
+					if (speed == 3) {// slowly
+
+						for (int i = 0; i <= 10; i++) {
+
+							cout << symbol;
+							Sleep(1000);
+						}
+					}
+
+
+				}
+			}
+		
+	}
+}
